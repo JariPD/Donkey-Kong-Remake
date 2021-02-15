5c4591faed9be28c1b5847ed2432f2ef44c1281a
JariPD <200502@student.glu.nl>
2021-02-15 14:00:16 +0100

    quit button, win area

--- Donkey Kong remake/Assets/Scripts/Menu.cs
+++ Donkey Kong remake/Assets/Scripts/Menu.cs
@@ -9,4 +9,10 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("you quit");
+    }
 }
---
+++ Donkey Kong remake/Assets/Scripts/WinCheck.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCheck : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            print("You win :D");
+        }
+    }
+
+}
